7dc3318eec8e671a4150d575506fe24c8de18713
Ken Dixon <ken.dixon@weather.com>
2019-08-21 17:43:50 -0400

    Update merge_interpolated_variables, but still not working for new variables

--- limited_area/mpas_to_mpas/merge_interpolated_variables.f90
+++ limited_area/mpas_to_mpas/merge_interpolated_variables.f90
@@ -1,84 +1,123 @@
-PROGRAM merge_fields
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! merge_fields
+! J. Cipriani
+! August / 2019
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Accepts 3 arguments
+!     1. Source NetCDF file
+!     2. Destination NetCDF file
+!     3. Variable name
+! The requested variable must be in the source NetCDF file, but not necessarily
+! in the destination NetCDF file.
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  use netcdf
+program merge_fields
 
-  IMPLICIT NONE
+    use netcdf
 
-  !Arguments
-  character(len = 500)                                 :: INCOMING_FCST
-  character(len = 500)                                 :: INTERP_INPUT
-  character(len = 500)                                 :: var
+    implicit none
 
-  integer                                              :: ncid, ncid_out
-  integer                                              :: nPoints, records, nz
-  integer                                              :: numdims, numatts
-  integer                                              :: varid, varid_out
-  integer, dimension(nf90_max_var_dims)                :: dimids
-  
-  real, dimension(:,:,:), allocatable                  :: tmp_3d
-  real, dimension(:,:), allocatable                    :: tmp_2d
-  real, dimension(:), allocatable                      :: tmp_1d
+    !Arguments
+    character(len = 500) :: source_file  ! DA output (source)
+    character(len = 500) :: dest_file    ! init.nc output (destination)
+    character(len = 500) :: var          ! Reqeusted variable/field
 
+    ! Variables
+    integer  :: ncid, ncid_out
+    integer  :: nPoints, nRecords, nz
+    integer  :: numdims, numatts
+    integer  :: varid, varid_out
+    integer, dimension(nf90_max_var_dims) :: dimids
+    logical  :: dest_has_var
+    real, dimension(:,:,:), allocatable :: tmp_3d
+    real, dimension(:,:), allocatable   :: tmp_2d
+    real, dimension(:), allocatable     :: tmp_1d
 
-! Get file name as argument
- call getarg(1,INTERP_INPUT)
- call getarg(2,INCOMING_FCST)
- call getarg(3,var)
-
-! Open netCDF files
- call check( nf90_open(trim(INTERP_INPUT), NF90_NOWRITE, ncid) )
- call check( nf90_open(trim(INCOMING_FCST), NF90_WRITE, ncid_out) )
-
-! Get varid's and VARIABLES from netCDF
- call check( nf90_inq_varid(ncid, trim(var), varid) )
- call check( nf90_inq_varid(ncid_out, trim(var), varid_out) )
- call check( nf90_inquire_variable(ncid, varid, ndims = numdims, natts = numatts) )
- call check( nf90_inquire_variable(ncid, varid, dimids = dimids) )
-
- if (trim(var) .ne. "xtime") then
-   ! 3 dimensions
-   if (numdims .eq. 3) then
-       call check( nf90_inquire_dimension(ncid, dimids(1), len = nz) )
-       call check( nf90_inquire_dimension(ncid, dimids(2), len = nPoints) )
-       call check( nf90_inquire_dimension(ncid, dimids(3), len = records) )
-       allocate(tmp_3d(nz,nPoints,records))
-       call check( nf90_get_var(ncid, varid, tmp_3d) )
-       call check( nf90_put_var(ncid_out,varid_out,tmp_3d) )
-       deallocate(tmp_3d)
-   ! 2 dimensions
-   else if (numdims .eq. 2) then
-       call check( nf90_inquire_dimension(ncid, dimids(1), len = nPoints) )
-       call check( nf90_inquire_dimension(ncid, dimids(2), len = records) )
-       allocate(tmp_2d(nPoints,records))
-       call check( nf90_get_var(ncid, varid, tmp_2d) )
-       call check( nf90_put_var(ncid_out,varid_out,tmp_2d) )
-       deallocate(tmp_2d)
-   ! 1 dimension
-   else
-       call check( nf90_inquire_dimension(ncid, dimids(1), len = nPoints) )
-       allocate(tmp_1d(nPoints))
-       call check( nf90_get_var(ncid, varid, tmp_1d) )
-       call check( nf90_put_var(ncid_out,varid_out,tmp_1d) )
-       deallocate(tmp_1d)
-   end if
-end if
+    ! only used if creating a new variable
+    integer  :: xtype
 
 
-! Close netCDF files
- call check( nf90_close(ncid) )
- call check( nf90_close(ncid_out) )
+    ! Process arguments
+    call getarg(1, source_file)    ! DA output (source)
+    call getarg(2, dest_file)      ! init.nc output (destination)
+    call getarg(3, var)            ! Requested variable/field
+    if (trim(var) .eq. "xtime") then
+        write(0, *) 'xtime not supported'
+        stop 3
+    end if
 
-! "Check" subroutine
-   CONTAINS
+    ! Open NetCDF Files
+    call check( nf90_open(trim(source_file), NF90_NOWRITE, ncid) )
+    call check( nf90_open(trim(dest_file), NF90_WRITE, ncid_out) )
 
-       SUBROUTINE check(status)
-         integer, intent (in) :: status
-         if(status .ne. nf90_noerr) then
-           print *, trim(nf90_strerror(status))
-           stop 2
-         end if
-       END SUBROUTINE check
+    ! Get IDs for requested variable
+    call check( nf90_inq_varid(ncid, trim(var), varid) )
+
+    ! Determine if we're writing a new variable to the destination
+    dest_has_var = .true.
+    if (nf90_inq_varid(ncid_out, trim(var), varid_out) .ne. NF90_NOERR) then
+        dest_has_var = .false.
+    end if
+
+    ! Get dimensions/attributes for source variable
+    call check( nf90_inquire_variable(ncid, varid, ndims = numdims, natts = numatts) )
+    call check( nf90_inquire_variable(ncid, varid, dimids = dimids) )
+    if (.not.dest_has_var) then
+        ! Get some more information necessary for variable creation
+        call check( nf90_inquire_variable(ncid, varid, xtype = xtype) )
+
+        ! Note that errors that appear here about "NC_UNLIMITED in the wrong
+        ! index" are likely due to differences in the format variant of NetCDF
+        ! (cdf5/64-bit-data, 64-bit-offset, etc.)
+        call check( nf90_redef(ncid_out) )  ! Go into redefine mode to create variable
+        write(*,*) 'Checkpoint-1'
+        write(*,*) 'dimids=',dimids(1:numdims)
+        call check( nf90_def_var(ncid_out, trim(var), xtype, dimids(1:numdims), varid_out) )
+        write(*,*) 'Checkpoint-22'
+
+        call check( nf90_enddef(ncid_out) )
+    end if
+
+    ! Perform merging (copying from source to destination)
+    ! 3 dimensions
+    if (numdims .eq. 3) then
+        call check( nf90_inquire_dimension(ncid, dimids(1), len = nz) )
+        call check( nf90_inquire_dimension(ncid, dimids(2), len = nPoints) )
+        call check( nf90_inquire_dimension(ncid, dimids(3), len = nRecords) )
+        allocate(tmp_3d(nz,nPoints,nRecords))
+        call check( nf90_get_var(ncid, varid, tmp_3d) )
+        call check( nf90_put_var(ncid_out, varid_out, tmp_3d) )
+        deallocate(tmp_3d)
+    ! 2 dimensions
+    else if (numdims .eq. 2) then
+        call check( nf90_inquire_dimension(ncid, dimids(1), len = nPoints) )
+        call check( nf90_inquire_dimension(ncid, dimids(2), len = nRecords) )
+        allocate(tmp_2d(nPoints,nRecords))
+        call check( nf90_get_var(ncid, varid, tmp_2d) )
+        call check( nf90_put_var(ncid_out, varid_out, tmp_2d) )
+        deallocate(tmp_2d)
+    ! 1 dimension
+    else
+        call check( nf90_inquire_dimension(ncid, dimids(1), len = nPoints) )
+        allocate(tmp_1d(nPoints))
+        call check( nf90_get_var(ncid, varid, tmp_1d) )
+        call check( nf90_put_var(ncid_out, varid_out, tmp_1d) )
+        deallocate(tmp_1d)
+    end if
+
+    ! Close netCDF files
+    call check( nf90_close(ncid) )
+    call check( nf90_close(ncid_out) )
 
 
-END PROGRAM merge_fields
+contains
 
+    subroutine check(status)
+        integer, intent (in) :: status
+        if(status .ne. nf90_noerr) then
+            print *, trim(nf90_strerror(status))
+            stop 2
+        end if
+    end subroutine check
+
+end program merge_fields
