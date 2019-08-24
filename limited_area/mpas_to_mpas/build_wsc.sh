5830c33a2eb54b776fea274402bdc702245585b8
James Cipriani <jpcipria@us.ibm.com>
2019-08-24 11:56:47 -0400

    modified build_wsc.sh

--- limited_area/mpas_to_mpas/build_wsc.sh
+++ limited_area/mpas_to_mpas/build_wsc.sh
@@ -10,6 +10,3 @@ if [ -f "a.out" ]; then
     ln -sf a.out ./mesh_to_mesh_interp
 fi
 
-# merge_fields
-echo "Building merge_fields"
-mpif90 merge_interpolated_variables.f90 -L${NETCDF}/lib -o merge_fields
