a050eeae8a889f7b5235c95ccf85067d8bbbb1f5
John Wong <john.wong@weather.com>
2020-03-25 16:21:18 -0400

    Replace 2d/3d indexing aliases with macros
    Attempt to reduce memory footprint by not generating arrays of pointers in mappers

--- limited_area/mpas_to_mpas/RemapperCell.cpp
+++ limited_area/mpas_to_mpas/RemapperCell.cpp
@@ -6,6 +6,12 @@
 #include "array_utils.hpp"
 #include "interp_utils.h"
 
+#define HSrcPts2d(i,j) HSrcPts[(i)*maxHSrcPts+(j)]
+#define HSrcWghts2d(i,j) HSrcWghts[(i)*maxHSrcPts+(j)]
+#define nVSrcPts2d(i,j) nVSrcPts[(i)*nVDstPts+(j)]
+#deinfe VSrcPts3d(i,j,k) VSrcPts[(i)*nVDstPts*maxVSrcPts+(j)*maxVSrcPts+(k)]
+#deinfe VSrcWghts3d(i,j,k) VSrcWghts[(i)*nVDstPts*maxVSrcPts+(j)*maxVSrcPts+(k)]
+
 RemapperCell::RemapperCell()
 {
 	nHDstPts = 0;
@@ -33,9 +39,7 @@ RemapperCell::RemapperCell(int nCellsDst, int nVertLevelsSrc, int nVertLevelsDst
     maxHSrcPts = vertexDegree;
     nHSrcPts = new int[nHDstPts];   // set to all 1 for now...
     HSrcPts = new int[(size_t)nHDstPts * (size_t)maxHSrcPts];
-    HSrcPts2d = allocate_2d<int>(nHDstPts, maxHSrcPts, HSrcPts);
     HSrcWghts = new float[(size_t)nHDstPts * (size_t)maxHSrcPts];
-    HSrcWghts2d = allocate_2d<float>(nHDstPts, maxHSrcPts, HSrcWghts);
     
     nVDstPts = nVertLevelsDst;
     nVSrcLevels = nVertLevelsSrc;
@@ -45,9 +49,7 @@ RemapperCell::RemapperCell(int nCellsDst, int nVertLevelsSrc, int nVertLevelsDst
         nVSrcPts = new int[(size_t)nHDstPts * (size_t)nVDstPts];
         nVSrcPts2d = allocate_2d<int>(nHDstPts, nVDstPts, nVSrcPts);
         VSrcPts = new int[(size_t)nHDstPts * (size_t)nVDstPts * (size_t)maxVSrcPts];
-        VSrcPts3d = allocate_3d<int>(nHDstPts, nVDstPts, maxVSrcPts, VSrcPts);
         VSrcWghts = new float[(size_t)nHDstPts * (size_t)nVDstPts * (size_t)maxVSrcPts];
-        VSrcWghts3d = allocate_3d<float>(nHDstPts, nVDstPts, maxVSrcPts, VSrcWghts);
     }
 }
 
@@ -57,26 +59,9 @@ RemapperCell::~RemapperCell()
 	if (HSrcPts != NULL) delete[] HSrcPts;
 	if (HSrcWghts != NULL) delete[] HSrcWghts;
 
-	if (HSrcPts2d != NULL) {
-		deallocate_2d<int>(HSrcPts2d);
-	}
-	if (HSrcWghts2d != NULL) {
-		deallocate_2d<float>(HSrcWghts2d);
-	}
-
 	if (nVSrcPts != NULL) delete[] nVSrcPts;
 	if (VSrcPts != NULL) delete[] VSrcPts;
 	if (VSrcWghts != NULL) delete[] VSrcWghts;
-
-	if (nVSrcPts2d != NULL) {
-		deallocate_2d<int>(nVSrcPts2d);
-	}
-	if (VSrcPts3d != NULL) {
-		deallocate_3d<int>(VSrcPts3d);
-	}
-	if (VSrcWghts3d != NULL) {
-		deallocate_3d<float>(VSrcWghts3d);
-	}
 }
 
 void RemapperCell::computeWeightsCell(int *nEdgesOnCellSrc, int **verticesOnCellSrc, int **cellsOnVertexSrc,
@@ -98,9 +83,9 @@ void RemapperCell::computeWeightsCell(int *nEdgesOnCellSrc, int **verticesOnCell
 		jCell = nearest_vertex(xCellDst[i], yCellDst[i], zCellDst[i], jCell, maxHSrcPts,
                            nEdgesOnCellSrc, verticesOnCellSrc, cellsOnVertexSrc,
                            xCellSrc, yCellSrc, zCellSrc, xVertexSrc, yVertexSrc, zVertexSrc);
-		HSrcPts2d[i][0] = cellsOnVertexSrc[jCell][0] - 1;
-		HSrcPts2d[i][1] = cellsOnVertexSrc[jCell][1] - 1;
-		HSrcPts2d[i][2] = cellsOnVertexSrc[jCell][2] - 1;
+		HSrcPts2d(i,0) = cellsOnVertexSrc[jCell][0] - 1;
+		HSrcPts2d(i,1) = cellsOnVertexSrc[jCell][1] - 1;
+		HSrcPts2d(i,2) = cellsOnVertexSrc[jCell][2] - 1;
         
         pointInterp[0] = xCellDst[i];
         pointInterp[1] = yCellDst[i];
@@ -120,13 +105,13 @@ void RemapperCell::computeWeightsCell(int *nEdgesOnCellSrc, int **verticesOnCell
 			}
 			for (int j=0; j<nHSrcPts[i]; j++) {
 				for (int k=0; k<nVSrcLevels; k++) {
-					tempLevels[k] += (HSrcWghts2d[i][j] * levelsSrc[HSrcPts2d[i][j]][k]);
+					tempLevels[k] += (HSrcWghts2d(i,j) * levelsSrc[HSrcPts2d(i,j)][k]);
 				}
 			}
 
 			// For each vertical destination point, determine weights from tempLevels
 			for (int k=0; k<nVDstPts; k++) {
-				get_weights_1d(nVSrcLevels, tempLevels, levelsDst[i][k], &nVSrcPts2d[i][k], VSrcPts3d[i][k], VSrcWghts3d[i][k]);
+				get_weights_1d(nVSrcLevels, tempLevels, levelsDst[i][k], &nVSrcPts2d(i,k), &VSrcPts3d(i,k,0), &VSrcWghts3d(i,k,0));
 			}
 		}
 	}
@@ -164,7 +149,7 @@ void RemapperCell::remap1D(float *dst, float *src)
 	for (int i=0; i<nHDstPts; i++) {
 		dst[i] = 0;
 		for (int j=0; j<nHSrcPts[i]; j++) {
-			dst[i] += (HSrcWghts2d[i][j] * src[HSrcPts2d[i][j]]);
+			dst[i] += (HSrcWghts2d(i,j) * src[HSrcPts2d(i,j)]);
 		}
 	}
 }
@@ -177,7 +162,7 @@ void RemapperCell::remap2D(float **dst, float **src)
 	for (int i=0; i<nHDstPts; i++) {
 		dst[0][i] = 0;
 		for (int j=0; j<nHSrcPts[i]; j++) {
-			dst[0][i] += (HSrcWghts2d[i][j] * src[0][HSrcPts2d[i][j]]);
+			dst[0][i] += (HSrcWghts2d(i,j) * src[0][HSrcPts2d(i,j)]);
 		}
 	}
 }
@@ -197,15 +182,15 @@ void RemapperCell::remap3D(float ***dst, float ***src)
 		}
 		for (int j=0; j<nHSrcPts[i]; j++) {
 			for (int k=0; k<nVSrcLevels; k++) {
-				tempLevels[k] += (HSrcWghts2d[i][j] * src[0][HSrcPts2d[i][j]][k]);
+				tempLevels[k] += (HSrcWghts2d(i,j) * src[0][HSrcPts2d(i,j)][k]);
 			}
 		}
 
 		// For each vertical destination point, interpolate
 		for (int k=0; k<nVDstPts; k++) {
 			dst[0][i][k] = 0;
-			for (int j=0; j<nVSrcPts2d[i][k]; j++) {
-				dst[0][i][k] += VSrcWghts3d[i][k][j] * tempLevels[VSrcPts3d[i][k][j]];
+			for (int j=0; j<nVSrcPts2d(i,k); j++) {
+				dst[0][i][k] += VSrcWghts3d(i,k,j) * tempLevels[VSrcPts3d(i,k,j)];
 			}
 		}
 	}
